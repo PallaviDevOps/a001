14473e02af00c858bc57428fbd952085899b979f
ph0013731 <ph0013731@INPUHJLP00060.TechMahindra.com>
2016-09-20 17:36:40 +0530

    First Commit 20Sep16

---
+++ A001/src/main/java/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ph0013731
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Maven World .... \n");
+	}
+
+}
